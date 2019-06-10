9f52c4acdcf232a147e98e6e6bcb796d853eea63
Administrator <Administrator@T450-LZX>
2019-06-10 15:30:34 +0800

    Custom Filter

--- hbase-action/src/main/java/com/hbase/learn/hbase_action/ch04/CustomFilterExample.java
+++ hbase-action/src/main/java/com/hbase/learn/hbase_action/ch04/CustomFilterExample.java
@@ -22,46 +22,53 @@ import com.hbase.learn.common.HBaseHelper;
 
 
 public class CustomFilterExample {
-	public static void main(String[] args) throws IOException {
-	    Configuration conf = HBaseConfiguration.create();
+      public static void main(String[] args) throws Exception {
+		
+    	  Configuration conf = HBaseConfiguration.create();
+  	    HBaseHelper helper = HBaseHelper.getHelper(conf);
+//  	    helper.dropTable("testtable");
+//  	    helper.createTable("testtable", "colfam1");
+//  	    System.out.println("Adding rows to table...");
+//  	    helper.fillTable("testtable", 1, 10, 10, 2, true, "colfam1");
 
-	    HBaseHelper helper = HBaseHelper.getHelper(conf);
-	    helper.dropTable("testtable");
-	    helper.createTable("testtable", "colfam1");
-	    System.out.println("Adding rows to table...");
-	    helper.fillTable("testtable", 1, 10, 10, 2, true, "colfam1");
+  	    Connection connection = ConnectionFactory.createConnection(conf);
+  	    Table table = connection.getTable(TableName.valueOf("testtable"));
+  	    // vv CustomFilterExample
+  	    List<Filter> filters = new ArrayList<Filter>();
 
-	    Connection connection = ConnectionFactory.createConnection(conf);
-	    Table table = connection.getTable(TableName.valueOf("testtable"));
-	    // vv CustomFilterExample
-	    List<Filter> filters = new ArrayList<Filter>();
+  	    Filter filter1 = new CustomFilter(Bytes.toBytes("val-05.05"));
+  	    filters.add(filter1);
 
-	    Filter filter1 = new CustomFilter(Bytes.toBytes("val-05.05"));
-	    filters.add(filter1);
+  	    Filter filter2 = new CustomFilter(Bytes.toBytes("val-02.07"));
+  	    filters.add(filter2);
 
-	    Filter filter2 = new CustomFilter(Bytes.toBytes("val-02.07"));
-	    filters.add(filter2);
+  	    Filter filter3 = new CustomFilter(Bytes.toBytes("val-09.01"));
+  	    filters.add(filter3);
 
-	    Filter filter3 = new CustomFilter(Bytes.toBytes("val-09.01"));
-	    filters.add(filter3);
+  	    FilterList filterList = new FilterList(
+  	      FilterList.Operator.MUST_PASS_ONE, filters);
 
-	    FilterList filterList = new FilterList(
-	      FilterList.Operator.MUST_PASS_ONE, filters);
+  	    Filter filterL = new CustomFilterL("row-07","row-08");
+  	    
+  	    Scan scan = new Scan();
+  	    scan.setFilter(filterL);
+  	    ResultScanner scanner = table.getScanner(scan);
+  	    // ^^ CustomFilterExample
+  	    System.out.println("Results of scan:");
+  	    // vv CustomFilterExample
+  	    for (Result result : scanner) {
+  	      for (Cell cell : result.rawCells()) {
+  	        System.out.println(
+  	        		"Cell: " + cell + ", Value: " +
+  	          Bytes.toString(cell.getValueArray(), cell.getValueOffset(),
+  	            cell.getValueLength()) +
+  	    	       ", Rowkey: " +  new String(cell.getRow())
 
-	    Scan scan = new Scan();
-	    scan.setFilter(filterList);
-	    ResultScanner scanner = table.getScanner(scan);
-	    // ^^ CustomFilterExample
-	    System.out.println("Results of scan:");
-	    // vv CustomFilterExample
-	    for (Result result : scanner) {
-	      for (Cell cell : result.rawCells()) {
-	        System.out.println("Cell: " + cell + ", Value: " +
-	          Bytes.toString(cell.getValueArray(), cell.getValueOffset(),
-	            cell.getValueLength()));
-	      }
-	    }
-	    scanner.close();
-	    // ^^ CustomFilterExample
-	  }
+  	        		);
+  	      }
+  	    }
+  	    scanner.close();
+  	    // ^^ CustomFilterExample
+    	  
+	}
 }
---
+++ hbase-action/src/main/java/com/hbase/learn/hbase_action/ch04/CustomFilterL.java
@@ -0,0 +1,94 @@
+package com.hbase.learn.hbase_action.ch04;
+
+import java.io.IOException;
+
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.exceptions.DeserializationException;
+import org.apache.hadoop.hbase.filter.Filter;
+import org.apache.hadoop.hbase.filter.FilterBase;
+import org.apache.hadoop.hbase.util.ByteStringer;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+public class CustomFilterL extends FilterBase {
+
+	private String start_row;
+	private String end_row;
+	private boolean filterRow = true;
+
+	public CustomFilterL() {
+		super();
+	}
+
+	public CustomFilterL(String start_row, String end_row) {
+		this.start_row = start_row;
+		this.end_row = end_row;
+	}
+
+	@Override
+	public boolean filterRowKey(byte[] buffer, int offset, int length) throws IOException {
+		// TODO Auto-generated method stub
+		filterRow = false;
+		return filterRow;
+	}
+
+	@Override
+	public ReturnCode filterKeyValue(Cell v) throws IOException {
+
+		if (new String(v.getRow()).compareToIgnoreCase(start_row) >= 0
+				&& new String(v.getRow()).compareToIgnoreCase(end_row) <= 0) {
+			return ReturnCode.INCLUDE;
+		}
+
+		   return ReturnCode.NEXT_ROW;
+	};
+
+	@Override
+	public boolean filterRow() throws IOException {
+		// TODO Auto-generated method stub
+		return filterRow;
+	}
+
+	@Override
+	public void reset() throws IOException {
+		// TODO Auto-generated method stub
+		super.reset();
+
+		this.filterRow = true;
+	}
+
+	@Override
+	public boolean filterAllRemaining() throws IOException {
+		// TODO Auto-generated method stub
+		return super.filterAllRemaining();
+	}
+
+	@Override
+	public byte[] toByteArray() {
+		FilterProtosL.CustomFilterL.Builder builder = FilterProtosL.CustomFilterL.newBuilder();
+		if (start_row != null)
+			builder.setStartRow(start_row);
+
+		if (end_row != null)
+			builder.setEndRow(end_row);
+
+		return builder.build().toByteArray();
+	}
+
+	// @Override
+	public static Filter parseFrom(final byte[] pbBytes) throws DeserializationException {
+		FilterProtosL.CustomFilterL proto;
+		try {
+			proto = FilterProtosL.CustomFilterL.parseFrom(pbBytes);
+		} catch (InvalidProtocolBufferException e) {
+			throw new DeserializationException(e);
+		}
+		return new CustomFilterL(
+				proto.getStartRow(), proto.getEndRow()
+//				Bytes.toString(proto.getStartRowBytes().toByteArray()),
+//				Bytes.toString(proto.getEndRowBytes().toByteArray())				
+				);
+	}
+
+}

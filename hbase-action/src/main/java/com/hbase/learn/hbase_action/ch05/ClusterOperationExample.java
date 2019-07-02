7fcfcfc7d14856c87f1abc2282c652b0201c0f55
Administrator <Administrator@T450-LZX>
2019-07-02 17:25:28 +0800

    ClusterOperationExample

--- hbase-action/src/main/java/com/hbase/learn/hbase_action/ch05/ClusterOperationExample.java
+++ hbase-action/src/main/java/com/hbase/learn/hbase_action/ch05/ClusterOperationExample.java
@@ -2,6 +2,8 @@ package com.hbase.learn.hbase_action.ch05;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -9,10 +11,14 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.ClusterStatus;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HRegionInfo;
+import org.apache.hadoop.hbase.HRegionLocation;
+import org.apache.hadoop.hbase.ServerName;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.RegionLocator;
 import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
 
 import com.hbase.learn.common.HBaseHelper;
 
@@ -26,27 +32,36 @@ public class ClusterOperationExample {
 		Table table = conn.getTable(tableName);
 		Admin admin = conn.getAdmin();
 
+
 		List<HRegionInfo> regionInfos = admin.getTableRegions(tableName);
-
-        List<String> regionNames = new ArrayList<String>();
-        		
-		for (HRegionInfo regionInfo : regionInfos) {
-			regionNames.add(regionInfo.getRegionNameAsString());
-			System.out.println(regionInfo.getRegionNameAsString());
-		}
-		admin.compact(tableName);
-		admin.balancer(true);
-
-		//testtable_ep,,1561366262458.4ff303dc03af0dc5a6d466205b82341b.
-		for (String regionName :regionNames) {
-			//admin.assign(regionName.getBytes());
-			admin.unassign(regionName.getBytes(), true);
-			admin.move(regionName.getBytes(), regionName.getBytes());
-		}
+		byte[]  startKey= regionInfos.get(0).getStartKey();
 		
-		ClusterStatus status =  admin.getClusterStatus();
+		RegionLocator locator = conn.getRegionLocator(tableName);
+		HRegionLocation location = locator.getRegionLocation(startKey);
+		ServerName currentServerName = location.getServerName();
+		System.out.println("currentServerName :" + currentServerName);
+
+		List<String> regionNames = new ArrayList<String>();
+		Collection<ServerName>  serverNames = new ArrayList<ServerName>();
+
+		ClusterStatus status = admin.getClusterStatus();
+		serverNames =  status.getServers();
+		regionNames.add(regionInfos.get(0).getRegionNameAsString());
+		String Mv2serverName = serverNames.iterator().next().getServerName();
+
+		// admin.compact(tableName);
+		// admin.balancer(true);
+		String EncodedName =location.getRegionInfo().getEncodedName();
+
+		//admin.move("d314de21984d07c938d45674d4d5a370".getBytes(), Mv2serverName.getBytes());
+        //admin.move(EncodedName.getBytes(),"hadoop4,16020,1562033872534".getBytes());
 		
-		System.out.println(status.toString());
+		
+//		admin.unassign(regionNames.get(0).getBytes(), true);
+//		admin.assign(regionNames.get(0).getBytes());
+
+
+		
 		
 		table.close();
 		conn.close();
